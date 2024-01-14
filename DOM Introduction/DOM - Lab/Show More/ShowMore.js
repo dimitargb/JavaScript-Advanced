70a527a29ac105dc7f5c53db6c1ed9c8843f8127
Dimitar Damianov <114077179+dimitargb@users.noreply.github.com>
2024-01-14 14:54:39 +0200

    Add files via upload

---
+++ DOM Introduction/DOM - Lab/Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+    const text = document.getElementById('text');
+    text.style.display = 'inline';
+
+    const btn = document.getElementById('more');
+    btn.style.display = 'none';
+
+}

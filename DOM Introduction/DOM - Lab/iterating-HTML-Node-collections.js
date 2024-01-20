6df9e0ff5019d10fef07b0bd7842ef359d6c1f09
mitkogb <114077179+dimitargb@users.noreply.github.com>
2024-01-20 15:36:41 +0200

    Add

---
+++ DOM Introduction/DOM - Lab/iterating-HTML-Node-collections.js
@@ -0,0 +1,12 @@
+const elements = document.querySelectorAll('p');
+const firstElement = elements[0];
+
+for (let p of elements){
+    // iterate over all elements
+}
+
+// both collections can be converted to an arry!!!
+const elementArray = Arry.from(elements); // converting to array !!!
+ 
+// querySelectorAll - връща NodeList !!!
+// оатналите връщат HTML Collection

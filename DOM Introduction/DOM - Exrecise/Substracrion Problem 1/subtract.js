8707b8e5d36ab0d9aee0de354d7954479ee7134b
mitkogb <114077179+dimitargb@users.noreply.github.com>
2024-01-24 16:13:23 +0200

    Add

---
+++ DOM Introduction/DOM - Exrecise/Substracrion Problem 1/subtract.js
@@ -0,0 +1,10 @@
+function subtract() {
+    const firstInput = document.getElementById('firstNumber').value;
+    const secondInput =document.getElementById('secondNumber').value;
+
+    const resultDiv = document.getElementById('result');
+    const finalResult = Number(firstInput) - Number(secondInput);
+  
+    resultDiv.textContent = finalResult;
+    
+}

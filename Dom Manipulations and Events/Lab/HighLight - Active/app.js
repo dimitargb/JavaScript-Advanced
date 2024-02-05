e8c8f2986833c03697d175c445bcf7b564982655
mitkogb <114077179+dimitargb@users.noreply.github.com>
2024-02-05 16:37:44 +0200

    Add

---
+++ Dom Manipulations and Events/Lab/HighLight - Active/app.js
@@ -0,0 +1,14 @@
+function focused() {
+    const inputs = Array.from(document.querySelectorAll('input'));
+    
+        for (let input of inputs) {
+            input.addEventListener('focus', (event) => {
+                event.target.parentElement.className = 'focused';
+            });
+
+            input.addEventListener('blur', (event) => {
+                event.target.parentElement.className = '';
+            });
+        }
+
+    }

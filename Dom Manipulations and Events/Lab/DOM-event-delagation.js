c49bebdcd78c49c6f9ed10cc1fe108a461eb14cc
mitkogb <114077179+dimitargb@users.noreply.github.com>
2024-02-11 18:33:02 +0200

    Add

---
+++ Dom Manipulations and Events/Lab/DOM-event-delagation.js
@@ -0,0 +1,5 @@
+// capture faza - върви по DOM дървото от горе надолу;
+// bubbling faza - върви по DOM дървото от долу нагоре;
+
+// event.stopPropagation = спира evenTListenera  дa се качва нагоре по DOM дървото
+// event.stopImmidiatePropagation = спира всички eventLiteneri да се изпълняват нагоре;

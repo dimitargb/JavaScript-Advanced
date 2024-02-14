a19bc82e35ee97f96022c6539f57ce6e0aae45dc
mitkogb <114077179+dimitargb@users.noreply.github.com>
2024-02-14 18:50:06 +0200

    Add

---
+++ Dom Manipulations and Events/Lab/Eventhandler.js
@@ -0,0 +1,3 @@
+// addEventListener() - htmlRef.addEventListener('click', handler == или подедената функция )
+//removeEventListener() - htmlRef.removeEventListener('click', handler == или подедената функция )
+//  за да можем да го истрием е важно функцията да се пази в променлива!!!

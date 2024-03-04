a5df8516541a43dfc47994a449a6a5e502be4de7
mitkogb <114077179+dimitargb@users.noreply.github.com>
2024-03-04 14:58:17 +0200

    Add

---
+++ Object and composition/Object Lab/Objects-as-associative-arrays.js
@@ -0,0 +1,8 @@
+const phonebook = {
+    'Jhon': '003598884477',
+    'Michael': '003598884445',
+    'Pamela': '003598884425'
+}
+// 'name' = property names(string Indexes) = key;
+// 'Peter' = value;
+console.log(phonebook['Michael']);

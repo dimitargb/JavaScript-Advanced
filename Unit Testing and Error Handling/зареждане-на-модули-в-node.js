b48beaba3d4ab82026864d42603d2746cb1ac963
mitkogb <114077179+dimitargb@users.noreply.github.com>
2024-04-15 17:19:13 +0300

    Add

---
+++ Unit Testing and Error Handling/зареждане-на-модули-в-node.js
@@ -0,0 +1,7 @@
+// require() - is used to import modules
+// one file.js  is one module
+
+const http = require('http');
+// ако искаме да заредим файл който ние сме направили, трябва да кажем пътия на файла койот започва винаги с ./
+
+const myModule = require('./myModule.js')
